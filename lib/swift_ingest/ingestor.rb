d176070f8e2e08312cbddab60a66d8c431050776
astrilet <astrilet@gmail.com>
2018-01-10 13:11:11 -0700

    Trimming line that is too long

--- lib/swift_ingest/ingestor.rb
+++ lib/swift_ingest/ingestor.rb
@@ -69,12 +69,13 @@ class SwiftIngest::Ingestor
     return deposited_file unless @dbcon
 
     # update db with deposited file info
-    @dbcon.query("INSERT INTO archiveEvent(project, container, ingestTime, objectIdentifier, objectChecksum, objectSize)\
-            VALUES('#{@project}', '#{swift_container}', now(), '#{file_base_name}', '#{checksum}', \
-            '#{File.size(file_name)}')")
+    @dbcon.query("INSERT INTO archiveEvent(project, container, ingestTime, \
+                  objectIdentifier, objectChecksum, objectSize) \
+                  VALUES('#{@project}', '#{swift_container}', now(), '#{file_base_name}', '#{checksum}', \
+                  '#{File.size(file_name)}')")
     custom_metadata.each do |key, value|
       @dbcon.query("INSERT INTO customMetadata(eventId, propertyName, propertyValue) \
-                 VALUES(LAST_INSERT_ID(), '#{key}', '#{value}' )")
+                    VALUES(LAST_INSERT_ID(), '#{key}', '#{value}' )")
     end
 
     deposited_file
